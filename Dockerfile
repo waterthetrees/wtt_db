08323ab2509eefa02ca5c35dc962b7fc24388143
Rose <rommims@gmail.com>
2021-02-28 23:36:51 -0800

    working db with docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM postgres
 
 ADD init-db.sh /docker-entrypoint-initdb.d/
-ADD treedb.sql /docker-entrypoint-initdb.d/
-RUN ["chmod", "+x", "/docker-entrypoint-initdb.d/init-db.sh"]
+ADD treedb.sql /db-dumps//
+# RUN ["chmod", "+x", "/docker-entrypoint-initdb.d/init-db.sh"]
--- init-db.sh
+++ init-db.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-set -o nounset
-set -o errexit
-set -o pipefail
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER trees WITH ENCRYPTED PASSWORD 'trees3r4t';
+    CREATE DATABASE treedb;
+    GRANT ALL PRIVILEGES ON DATABASE treedb TO trees;
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+EOSQL
 
-POSTGRES_USER="${POSTGRES_USER:-postgres}"
-POSTGRES_PASSWORD="${POSTGRES_PASSWORD}"
-APP_POSTGRES_DB="${APP_POSTGRES_DB:-treedb}"
-APP_POSTGRES_USER="${APP_POSTGRES_USER:-trees}"
-APP_POSTGRES_PASSWORD="${APP_POSTGRES_PASSWORD:-trees3r4t}"
-SQL_FILENAME=${SQL_FILENAME:-treedb.sql}
+psql -f /db-dumps/treedb.sql treedb
 
-DATABASE="${APP_POSTGRES_DB}"
-
-# Create single database.
-psql --variable ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --command "CREATE DATABASE ${DATABASE}"
-
-# Create app user.
-psql --variable ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --command "CREATE USER ${APP_POSTGRES_USER} SUPERUSER PASSWORD '${APP_POSTGRES_PASSWORD}'"
-psql --variable ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --command "GRANT ALL PRIVILEGES ON DATABASE ${DATABASE} TO ${APP_POSTGRES_USER}"
-psql --variable ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --command "ALTER USER ${APP_POSTGRES_USER} CREATEDB;"
-psql --variable ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --command 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-psql --variable --username "${APP_POSTGRES_USER}" --dbname "${DATABASE}" < /docker-entrypoint-initdb.d/"$SQL_FILENAME"
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"  < /docker-entrypoint-initdb.d/treedb.sql
+echo "Copying data: ${POSTGRES_DB}"
