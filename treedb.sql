93fe520e94d422a0800fe78a7cfc1fc0f5c669b2
Victor Wong <victorw1992@gmail.com>
2021-05-21 14:55:52 -0700

    fixing water-timer triggers

--- treedb.sql
+++ treedb.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cf3f681bec792b53ded414e209efe0fc7ce22311ab9406754f72c4521ce934e
-size 113296599
+oid sha256:6e8bf2068714aec115b64c0baee869ef86cedaf212e80496b7a7baeef71604d3
+size 113296757
