8d01aec48321f510347d4319495f8afe235bb121
Rose <rommims@gmail.com>
2021-06-23 12:45:03 -0700

    treedb dump location fixes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM postgis/postgis
 
 ADD init-db.sh /docker-entrypoint-initdb.d/
+ADD treedb.sql /db-dumps/
 RUN ["chmod", "+x", "/docker-entrypoint-initdb.d/init-db.sh"]
--- treedb.sql
+++ treedb.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61809807675efce1a70de34226d39f4db044fd08267057d2adde61958dfde65b
-size 113295221
+oid sha256:504f192354e172eb5f8cdc6f02d0c95031501dc67219cbd5fdd42347c5c4fe27
+size 113295495
