27549fc5377b51b2336fac5331336271f1ddc69a
Anthony Chian <anthonychian@gmail.com>
2021-09-22 20:25:31 -0700

    Add lng and lat to countries table

--- treedb.sql
+++ treedb.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e8bf2068714aec115b64c0baee869ef86cedaf212e80496b7a7baeef71604d3
-size 113296757
+oid sha256:85ffaecf75c629f796fd9dab29b9899c496afce8f51a7eccb27048c8592fc4ab
+size 113298071
