7f583d1ce366dba8cafcd16b23885d37c2f5f55c
zoobot <rommims@gmail.com>
2023-02-14 09:25:45 -0800

    treedata cleanup

--- migrations/2023-02-14-migration.sql
+++ migrations/2023-02-14-migration.sql
@@ -5,7 +5,7 @@ ALTER TABLE "crosswalk" RENAME COLUMN "ref" to id_reference;
 
 ALTER TABLE "sources" DROP COLUMN id_city_name;
 ALTER TABLE "cities" DROP COLUMN id_source;
-
+ALTER TABLE "sources" DROP COLUMN id_name2;
 
 ALTER TABLE "treedata" ADD dbh_min INTEGER;
 ALTER TABLE "treedata" ADD dbh_max INTEGER;
@@ -13,3 +13,18 @@ ALTER TABLE "treedata" ADD height_min INTEGER;
 ALTER TABLE "treedata" ADD height_max INTEGER;
 ALTER TABLE "treedata" ADD crown_min INTEGER;
 ALTER TABLE "treedata" ADD crown_max INTEGER;
+
+
+
+ALTER TABLE "treedata" DROP COLUMN iso_alpha_2;
+ALTER TABLE "treedata" DROP COLUMN iso_alpha_3;
+ALTER TABLE "treedata" DROP COLUMN numeric_country_code;
+ALTER TABLE "treedata" DROP COLUMN info;
+ALTER TABLE "treedata" DROP COLUMN download;
+ALTER TABLE "treedata" RENAME COLUMN "source_id" to id_source_name;
+
+
+-- MAKE SURE YOU RUN THIS BEFORE YOU RUN THE NEXT ONE
+UPDATE "treedata" SET id_reference = ref WHERE ref IS NOT NULL;
+ALTER TABLE "treedata" DROP COLUMN ref;
+ALTER TABLE "treedata" DROP COLUMN red;
